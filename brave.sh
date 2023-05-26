5f3fe10658c8061b102800fd0bddc7b48ad32655
Anduriil <134679667+Anduriil@users.noreply.github.com>
2023-05-26 12:27:41 +0300

    sh
    bash

---
+++ brave.sh
@@ -0,0 +1,6 @@
+echo -e '\033[0;31m'Requires Brave Browser installed.
+echo -e '\033[0;32m'Download Brave:
+echo -e '\033[0;34m''     https://brave.com/download''\033[0m'
+echo -e '\033[0;34m'"Launching Brave browser if it's installed...."
+echo -e '\033[0;34m'"Exiting..."
+brave-browser --app=https://anduriil.github.io/  &! exit
---
+++ chrome.sh
@@ -0,0 +1,6 @@
+echo -e '\033[0;31m'Requires Google Chrome Browser installed.
+echo -e '\033[0;32m'Download Google Chrome:
+echo -e '\033[0;34m''     https://google.com/chrome''\033[0m'
+echo -e '\033[0;34m'"Launching Chrome browser if it's installed...."
+echo -e '\033[0;34m'"Exiting..."
+google-chrome --app=https://anduriil.github.io/  &! exit
