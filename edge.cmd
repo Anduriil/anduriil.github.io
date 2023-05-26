0b59d6903e4ba13df31677f459214cfbbf814a13
Anduriil <134679667+Anduriil@users.noreply.github.com>
2023-05-26 15:09:19 +0300

    Update edge.cmd

--- edge.cmd
+++ edge.cmd
@@ -1,4 +1,2 @@
-@echo off
-set username=%username%
-start "" "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --app=https://anduriil.github.io
+"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --app=https://anduriil.github.io
 exit
