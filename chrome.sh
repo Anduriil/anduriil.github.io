6ee1fb45d07a0acbc6421d70c9ca7ac2f620d7b7
Anduriil <134679667+Anduriil@users.noreply.github.com>
2023-10-21 10:32:15 +0300

    Update chrome.sh

--- chrome.sh
+++ chrome.sh
@@ -3,4 +3,4 @@ echo -e '\033[0;32m'Download Google Chrome:
 echo -e '\033[0;34m''     https://google.com/chrome''\033[0m'
 echo -e '\033[0;34m'"Launching Chrome browser if it's installed...."
 echo -e '\033[0;34m'"Exiting..."
-google-chrome --app=https://anduriil.github.io/  &! exit
+google-chrome --app=https://valiant-sapphire-trillium.glitch.me/  &! exit
