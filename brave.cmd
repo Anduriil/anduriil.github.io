64beedb2052320ccfcc1f48deb39ffb040acf0ee
Anduriil <134679667+Anduriil@users.noreply.github.com>
2023-10-21 10:31:28 +0300

    Update brave.cmd

--- brave.cmd
+++ brave.cmd
@@ -3,10 +3,10 @@ set "chromeExe32=%ProgramFiles%\BraveSoftware\Brave-Browser\Application\brave.ex
 set "chromeExe64=%ProgramFiles(x86)%\BraveSoftware\Brave-Browser\Application\brave.exe"
 
 if exist "%chromeExe32%" (
-    start "" "%chromeExe32%" --app=https://anduriil.github.io/
+    start "" "%chromeExe32%" --app=https://valiant-sapphire-trillium.glitch.me/
     exit
 ) else if exist "%chromeExe64%" (
-    start "" "%chromeExe64%" --app=https://anduriil.github.io/
+    start "" "%chromeExe64%" --app=https://valiant-sapphire-trillium.glitch.me/
     exit
 ) else (
     color 0c
