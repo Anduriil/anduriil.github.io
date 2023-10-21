ae366c9902972223ab0f41f89fd6b2f32e1b160c
Anduriil <134679667+Anduriil@users.noreply.github.com>
2023-10-21 10:32:02 +0300

    Update chrome.cmd

--- chrome.cmd
+++ chrome.cmd
@@ -3,10 +3,10 @@ set "chromeExe32=%ProgramFiles%\Google\Chrome\Application\chrome.exe"
 set "chromeExe64=%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe"
 
 if exist "%chromeExe32%" (
-    start "" "%chromeExe32%" --app=https://anduriil.github.io/
+    start "" "%chromeExe32%" --app=https://valiant-sapphire-trillium.glitch.me/
     exit
 ) else if exist "%chromeExe64%" (
-    start "" "%chromeExe64%" --app=https://anduriil.github.io/
+    start "" "%chromeExe64%" --app=https://valiant-sapphire-trillium.glitch.me/
     exit
 ) else (
     color 0c
